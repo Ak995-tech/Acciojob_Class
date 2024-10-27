d2dd9d5ffb8f5d91ccd7875299ecb41da3a23d6f
Akash Kumar <106898410+Ak995-tech@users.noreply.github.com>
2024-10-27 10:14:05 +0530

    variable and data type
    sum

---
+++ Variable_and_data_type/sum.java
@@ -0,0 +1,14 @@
+package Acciojob_Topics.Variable_and_data_type;
+
+public class sum {
+    public static void main(String [] args){
+        int a = 15;
+        int b = 16;
+        int add = a+b;
+        int mul = a*b;
+        int diff = a-b;
+        System.out.println(add);
+        System.out.println(mul);
+        System.out.println(diff);
+    }
+}
