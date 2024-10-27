0713a9a4712b6a643f94a3a21d0ed2d5b14e9e4b
Akash Kumar <106898410+Ak995-tech@users.noreply.github.com>
2024-10-27 10:24:23 +0530

    variable_and_data_type/conversion.java
    conversion

---
+++ Variable_and_data_type/conversion.java
@@ -0,0 +1,11 @@
+package Acciojob_Topics.Variable_and_data_type;
+
+public class conversion {
+    public static void main (String[] args){
+int a = 5;
+long b = 66202;
+long mul = a*b;
+System.out.println(mul);
+    }
+    
+}
